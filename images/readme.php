81a602f53b3b1435d75bd677244ea0ff04444f77
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-03-10 11:46:22 +0700

    versi1

---
+++ images/readme.php
@@ -0,0 +1,9 @@
+<!--
+	Name        : Modul Kuesioner Kepuasan pasien
+	Description : Sistem informasi kuesioner kepuasan pasien
+	Date        : 2019
+	Developer   : Ardi Tri Heru
+	Phone/WA    : 0896-2967-1717
+	Email       : arditriheruh@gmail.com
+	Website     : https://www.arditriheru.com
+-->
